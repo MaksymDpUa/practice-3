336c9456fc328a3744dfed4cb568eadc00bbd645
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-07-08 16:15:49 +0300

    Add country page

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -1,11 +1,15 @@
 import { Grid, GridItem } from 'components';
+import { Link, useLocation } from 'react-router-dom';
 
 export const CountryList = ({ countries }) => {
+  const location = useLocation();
   return (
     <Grid>
       {countries.map(countrie => (
         <GridItem key={countrie.country}>
-          <img src={countrie.flag} alt={countrie.country} />
+          <Link to={`/country/${countrie.id}`} state={{from: location}}>
+            <img src={countrie.flag} alt={countrie.country} />
+          </Link>
         </GridItem>
       ))}
     </Grid>
